723e8c88daad75332fb7d77d97cc7ea6b66bdb89
Dejan Zelic <dejanzelic@gmail.com>
2017-12-29 11:06:16 -0700

    added simple template

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var users = require('./routes/simple');
+var simple = require('./routes/simple');
 
 var app = express();
 
